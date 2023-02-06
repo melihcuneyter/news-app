7b3ed1436e2b922a9fdd9152e2d9f31ede55978b
Melih Cüneyter <melihcuneyter@hotmail.com>
2023-02-06 14:18:16 +0300

    Initial project.

---
+++ news-app/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  news-app
+//
+//  Created by Melih Cüneyter on 6.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
